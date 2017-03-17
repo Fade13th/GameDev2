7e762e8624c21467e2cfecb9f14836445a8a3e77
Matt Andrew <meja1g14@soton.ac.uk>
2017-03-17 16:52:28 +0000

    Added player sprite and animator

---
+++ Assets/Sprites/PlayerAnim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GameObject.Find("Player").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A)) {
+            anim.SetInteger("Direction", 1);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            anim.SetInteger("Direction", 3);
+        }
+        else if (Input.GetKey(KeyCode.W)) {
+            anim.SetInteger("Direction", 2);
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            anim.SetInteger("Direction", 4);
+        }
+        else {
+            anim.SetInteger("Direction", 0);
+        }
+    }
+}
