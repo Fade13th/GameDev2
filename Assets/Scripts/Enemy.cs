bb1ce5bea75831c0c7e6f768ff07414b7aad5746
Matt Andrew <meja1g14@soton.ac.uk>
2017-03-18 00:53:34 +0000

    Basic movement and minimal randomised enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed;
+    private float moveVelocity;
+
+    public Animator anim;
+
+    private System.Random rand;
+
+    private int lastDirection = -1;
+
+    public float walkTimeUpper;
+    public float walkTimeLower;
+    public int walkChance = 100;
+    private bool walking = false;
+    private float time;
+
+    private float currentVelocity = 0;
+
+    // Use this for initialization
+    void Start () {
+        rand = new System.Random();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        moveVelocity = 0;
+
+        if (walking) {
+            if (time > Time.time) {
+                moveVelocity = currentVelocity; 
+            }
+            else {
+                currentVelocity = 0;
+                walking = false;
+                anim.SetInteger("Direction", 0);
+            }
+        }
+
+        //Moves back and forth a set amount at random intervals
+        if (!walking && rand.Next(0, walkChance) == 1) {
+            lastDirection = -lastDirection;
+            moveVelocity += lastDirection * speed;
+
+            if (lastDirection > 0) {
+                anim.SetInteger("Direction", 3);
+            }
+            else {
+                anim.SetInteger("Direction", 1);
+            }
+
+            currentVelocity = moveVelocity;
+            walking = true;
+            time = Time.time + (float)( rand.NextDouble() * (walkTimeUpper - walkTimeLower) + walkTimeLower);
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //Movement
+    public float speed;
+    public float jump;
+    float moveVelocity;
+
+    public Animator anim;
+
+    //Grounded Vars
+    bool isGrounded = true;
+
+    void Update() {
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W)) {
+            if (isGrounded) {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+                isGrounded = false;
+            }
+        }
+
+        moveVelocity = 0;
+
+        //Left Right Movement
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+            moveVelocity = -speed;
+            anim.SetInteger("Direction", 1);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            moveVelocity = speed;
+            anim.SetInteger("Direction", 3);
+        }
+        else
+            anim.SetInteger("Direction", 0);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+    //Check if Grounded
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.tag == "Floor")
+            isGrounded = true;
+    }
+}
