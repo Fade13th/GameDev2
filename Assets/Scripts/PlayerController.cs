283ff6a205fa03962715d84c19510834b187a4ea
Mark <m@aqu.me.uk>
2017-03-20 13:43:01 +0000

    Made Goats not likeing walking on their faces after falling/being pushed off ledges

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,29 +7,41 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public float jump;
     float moveVelocity;
+    public bool JumpButtonPressed { get; set; }
 
     public Animator anim;
 
     //Grounded Vars
     bool isGrounded = true;
 
+    void Start()
+    {
+        JumpButtonPressed = false;
+    }
+
     void Update() {
         //Jumping
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W)) {
-            if (isGrounded) {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
-                isGrounded = false;
-            }
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W)) {
+            JumpButtonPressed = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.Z) || Input.GetKeyUp(KeyCode.W)) {
+            JumpButtonPressed = false;
+        }
+
+        if(JumpButtonPressed && isGrounded) {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+            isGrounded = false;
+            JumpButtonPressed = false;
         }
 
         moveVelocity = 0;
 
         //Left Right Movement
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
             moveVelocity = -speed;
             anim.SetInteger("Direction", 1);
         }
-        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+        else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
             moveVelocity = speed;
             anim.SetInteger("Direction", 3);
         }
@@ -41,7 +53,7 @@ public class PlayerController : MonoBehaviour {
     }
     //Check if Grounded
     void OnTriggerEnter2D(Collider2D collider) {
-        if (collider.tag == "Floor")
+        if(collider.tag == "Floor")
             isGrounded = true;
     }
 }
