50c6cb9df6db27d597c643bd4bc3b5b42e0b76c7
Mark <m@aqu.me.uk>
2017-03-20 14:28:22 +0000

    Renamed FovScript to FovController

---
+++ Assets/Scripts/FovController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FovController : MonoBehaviour {
+
+    public bool PlayerSeen { get; set; }
+
+    private Color _playerVisible, _playerNotVisible;
+
+	// Use this for initialization
+	void Start () {
+		_playerVisible = Color.red;
+		_playerVisible.a = 60f/255f;
+		_playerNotVisible = Color.green;
+		_playerNotVisible.a = 60f/255f;
+        GetComponent<SpriteRenderer>().color = _playerNotVisible;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            PlayerSeen = true;
+            GetComponent<SpriteRenderer>().color = _playerVisible;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Player") { 
+            PlayerSeen = false;
+            GetComponent<SpriteRenderer>().color = _playerNotVisible;
+        }
+}
+
+}
