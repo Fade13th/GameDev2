e8b1a1a5df68a95a2b4697b4b1b57deaf6f98a10
Mark Powell <Mark Powell>
2017-03-31 18:34:20 +0100

    Added mising files

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+    public float tileSizeX, tileSizeY;
+
+    private Vector2 playerStartPosition;
+    private Vector2 startPosition;
+    private PlayerController player;
+
+    void Start()
+    {
+        player = PlayerController.GetPlayer();
+        playerStartPosition = player.transform.position;
+
+        startPosition = transform.position;
+    }
+
+    void Update() {
+        float xOffset = Mathf.Repeat(player.transform.position.x * 0.1f, 2* tileSizeX);
+        float yOffset = (player.transform.position.y - playerStartPosition.y) * 0.9f + startPosition.y;
+        transform.position = player.transform.position + Vector3.left * (xOffset - tileSizeX) + Vector3.up * (yOffset - player.transform.position.y);
+    }
+}
