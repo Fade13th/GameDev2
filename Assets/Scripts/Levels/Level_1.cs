efaa74f5b9551bd7fd5f81391c85327a705b7fdf
Matt Andrew <meja1g14@soton.ac.uk>
2017-03-31 17:13:08 +0100

    Added progression scripts ready to link to the ui

---
+++ Assets/Scripts/Levels/Level_1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_1 : Level {
+
+    protected override void updateObj() {
+        //Add stuff to manage ui when implemented
+        print(stage);
+    }
+}
