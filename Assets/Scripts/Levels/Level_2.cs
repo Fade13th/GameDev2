05500346ea069af429911fa97d8c756c836e7596
Matt Andrew <meja1g14@soton.ac.uk>
2017-04-01 19:38:48 +0100

    updated level 2

---
+++ Assets/Scripts/Levels/Level_2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_2 : Level {
+    public bool crooked = false;
+
+    protected override void updateObj() {
+        base.updateObj();
+        switch (stage) {
+            case 0:
+                if (!crooked)
+                    objectiveText.text = "Objective: Retreive camera footage from the CCTV computer";
+                else
+                    objectiveText.text = "Objective: Plant incriminating files on the CCTV computer";
+                break;
+
+            case 1:
+                objectiveText.text = "Objective: Exit through the window you entered";
+                break;
+
+            default:
+                break;
+        }
+    }
+}
